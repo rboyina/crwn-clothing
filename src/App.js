eb3e5a290015f961d84755e6a78404c5d7177566
Rohit Boyina <25886790+rboyina@users.noreply.github.com>
2020-04-05 20:21:42 -0400

    Added router package and used WithRouter for menucomponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Homepage from './Pages/homepage/homepage.component';
-
+import {Route} from 'react-router-dom';
 import './App.css';
+
+
+const HatsPage= (props) =>(
+  console.log(props),
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage/>
+        <Route exact path='/' component={Homepage}></Route>
+        <Route path='/hats' component={HatsPage}></Route>
     </div>
   );
 }
--- src/Pages/homepage/homepage.component.jsx
+++ src/Pages/homepage/homepage.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../Components/Directory/directory.component';
 
-const Homepage = () => (
+const Homepage = (props) => (
+    console.log(props),
 <div className='homepage'>
     <Directory/>
 </div>
